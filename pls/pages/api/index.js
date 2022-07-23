399167df10b7f3f4194122c7d66684931727ccc8
Jackson Camp <jacksoncamp@Jacksons-MacBook-Pro.local>
2022-07-22 20:21:56 -0700

    final

---
+++ pls/pages/api/index.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const url =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          "x-rapidapi-key":
+            "40a5626f0amshb9007a31dcbf94fp10f815jsn9e58e2782e5c",
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(`Fetch data Error : ${error}`);
+  }
+};
